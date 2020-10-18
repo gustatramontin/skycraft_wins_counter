5b1767ed3321612f258e6c52ab393e4439bd51ff
Gustavo Tramontin <mrgustavotramontin@gmail.com>
2020-10-18 15:41:06 -0300

    remade the code form that commands are made

--- bot.py
+++ bot.py
@@ -3,65 +3,80 @@ import re
 
 import discord
 from dotenv import load_dotenv
+from discord.ext.commands import Bot
 
 from manage_data import manage
 from db import Sqlite
-from interface import update
+from interface import update_datas
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
+PREFIX = '?'
+
 client = discord.Client()
 
-@client.event
-async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+bot = Bot(command_prefix = PREFIX)
 
-@client.event
-async def on_message(msg):
-    if msg.author == client.user:
-        return
-    
-    if re.search('^§rank ', msg.content):
-        try:
-            try:
-                page = int(msg.content[6:8])
-                if page > 61:
-                    await msg.channel.send('Páginas maiores que 61 não estão disponivéis ainda.')
-                    return
-            except:
-                page = int(msg.content[6])
-        except:
-            await msg.channel.send("Escreva da maneira correta §rank <página>")
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has connected to Discord!')
+
+@bot.command()
+async def commands(ctx):
+    embed = discord.Embed(title="Lista de Comandos", description=f"Uma lista dos Comandos disponíveis", color=0xff1a1a)
+
+    comandos = f"""```
+rank <página> | mostra o rank de vitorias;
+find <nome_de_usuario> | mostras as vitórias do jogador marcado;
+update | atualiza o rank;
+sourcecode
+```    
+"""
+
+    embed.add_field(name="Comando    Função", value=comandos)
+
+    await ctx.channel.send(embed=embed)
+
+@bot.command()
+async def rank(ctx, page):
+    if page.isnumeric() and int(page) < 61:
+
+        page = int(page)
 
         datas = manage.show_wins(False)
-        
+            
         datas = datas[(page-1)*20:page*(20)]
 
         message_of_wins = ''
         for row in datas:
             message_of_wins += (f'{row[0]} | {row[1]}\n')
 
-
         embed = discord.Embed(title="Vitórias dos Jogadores", description=f"Rank das vitórias página {page}", color=0xff1a1a)
         embed.add_field(name="Nome    Vitórias", value=message_of_wins)
-        embed.add_field(name="AD", value="Inscreva-se no Canal do Felipe Neto: https://www.youtube.com/felipeneto/")
+        embed.set_image(url='https://skycraft.com.br/images/games/blockparty.png')
 
-        await msg.channel.send(embed=embed)
+        await ctx.channel.send(embed=embed)
+    else:
+        await ctx.channel.send(str(ctx.author.mention) + ', você escreveu o parâmetro do comando de maneira incorreta.')
 
-    if re.search('^§find ', msg.content):
+@bot.command()
+async def update(ctx):
+    update_datas()
+    await ctx.channel.send('Dados atualizados.')
 
-        name = msg.content[6:]
-        datas = Sqlite.query(f"select wins from rank where username='{name}'")
-        try:
-            await msg.channel.send(f'{name} : {datas[0][0]}')
-        except:
-            await msg.channel.send('Esse nome não existe ou foi digitado de maneira incorreta!')
-    
-    if msg.content == '§update':
-        update()
-        await msg.channel.send('Dados atualizados.')
+@bot.command()
+async def find(ctx, name):
+    datas = Sqlite.query(f"select wins from rank where username='{name}'")
 
+    try:
+        await ctx.channel.send(f'{name} : {datas[0][0]}')
+    except:
+        await ctx.channel.send('Esse nome não existe ou foi digitado de maneira incorreta!')
 
-client.run(TOKEN)
+@bot.command()
+async def sourcecode(ctx):
+    await ctx.channel.send('https://github.com/gustatramontin/skycraft_wins_counter/tree/master')
+
+bot.run(TOKEN)
