553555a7c7fe29d7a8b41c730c36da3cbaaf8097
Gustavo Tramontin <mrgustavotramontin@gmail.com>
2020-10-31 11:56:04 -0300

    moved to modules folder

---
+++ bot/bot_modules/chart.py
@@ -0,0 +1,42 @@
+import matplotlib.pyplot as plt
+from Rank import rank
+from discord import File
+
+class RankChart:
+
+    def __init__(self, channel):
+        self.channel = channel # discord channel to show the charts
+
+    def get_wins(self):
+        data = rank.show_wins(10)
+        return data
+
+    async def diplay(self):
+        await self.delete()
+        wins_and_names =self.get_wins()
+        names = []
+        wins = []
+        labels = []
+
+        for name, win in wins_and_names:
+            names.append(name)
+            wins.append(win)
+            labels.append(f"{name}({win})")
+
+        plt.pie(wins, labels=labels)
+        plt.savefig("chart.png")
+        plt.close()
+
+        image = File("chart.png")
+
+        await self.channel.send(file=image)
+
+
+    async def delete(self):
+        try:
+            messages = await self.channel.history().flatten()
+            for message in messages: 
+                await message.delete()
+        except:
+            print('No laste message')
+ 
